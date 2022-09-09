ed121b924509f7f00ac8b89db9124cfaae429673
Icongram <doer_tenant_od@icloud.com>
2022-09-08 20:44:26 -0700

    printing with function puts included

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, none otherwise
+ */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
