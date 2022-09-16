cbbd969c29cad919d2fb467aedcd0412e58a2406
Icongram <doer_tenant_od@icloud.com>
2022-09-16 15:10:36 -0700

    task 0-5

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * a function that checks for a digit (0 through 9).
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	return (c >= '0' && c <= '9');
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * a function that multiplies two integers.
+ * mul(int a, int b);
+ * Return: the result
+ */
+int mul(int a, int b);
+{
+	int c;
+
+	c = a * b;
+	return(c);
+}
