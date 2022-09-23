559216ce6c499afdd34114ec1505ab2b614990c9
Icongram <doer_tenant_od@icloud.com>
2022-09-23 13:42:17 -0700

    a function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,11 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		-putchar('+');
+		_putchar('+');
 		return (1);
-	}else if (n = 0)
+	}else if (n == 0)
 	{
-		-putchar('0');
+		-putchar(48);
 		return (0);
 	}else if (n < 0)
 	{
