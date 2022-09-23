167f21eb17a4cfedc1922f9c8afe579f3f42224e
Icongram <doer_tenant_od@icloud.com>
2022-09-23 13:27:00 -0700

    a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: the int to check
+ * Return: 1 and print + if n is > zero
+ * 0 and print 0 if n is = zero
+ * -1 and print - if n is < zero.
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		-putchar('+');
+		return (1);
+	}else if (n = 0)
+	{
+		-putchar('0');
+		return (0);
+	}else if (n < 0)
+	{
+		-putchar('-');
+		return (-1);
+	}
