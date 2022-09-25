b85efca81dd52d03f48aa192fd5a5bbbc1a00d19
Icongram <doer_tenant_od@icloud.com>
2022-09-25 04:10:35 -0700

    a function that prints numbers from 0 - 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-
-		if (j <= 14)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
-			j++;
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
