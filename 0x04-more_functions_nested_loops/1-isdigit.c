927566c91a00ef227b85c704f7531f8f1c77800a
Icongram <doer_tenant_od@icloud.com>
2022-09-25 02:20:21 -0700

    README.md and header file update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * a function that checks for a digit (0 through 9).
- * Return: 1 if c is a digit, 0 otherwise.
- */
-int _isupper(int c)
-{
-	return (c >= '0' && c <= '9');
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Learning more about more use of functions and nested loops
+more on nested loops and functions
