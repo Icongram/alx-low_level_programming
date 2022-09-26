93082a0ffc3296ebc55d22aad240dfa5ac9041a2
Icongram <doer_tenant_od@icloud.com>
2022-09-25 23:58:35 -0700

    a funtion that updates the value of avariable through the pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of the variable
+ * the pointer points to 98
+ * @n: pointer to the variable to updater
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
